dfa6ddb26541954e43fdd41af5c6986887524027
asanotomoki <mirai20190820@gmail.com>
2020-05-19 18:14:40 +0900

    porpTypes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  render() {
-    const gree = "it's a small world";
-    const dom = <p className="foo">{gree}</p>;
-    return(
-      <input
-        type="text"
-        onChange={() => {
-          alert("MY name is");
-        }}
-      />
-    );
-  }
+const App = () => {
+  const profiles = [
+    { name:"ともき", age: 17 },
+    { name:"rin", age: 17 },
+    {  }
+  ]
+
+  return (
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age}years old!</div>
+};
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired
+}
+
+User.defaultProps= {
+  name: "NoName",
+  age: 0
 }
 
 export default App;
