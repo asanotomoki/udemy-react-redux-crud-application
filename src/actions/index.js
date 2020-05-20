5e8c64fcf002adf0b6c189c36966e3a21aa43d66
asanotomoki <mirai20190820@gmail.com>
2020-05-20 21:54:23 +0900

    increment decrement

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const increment = "INCREMENT";
+export const decrement = "DECREMENT";
+export const increment = () => ({
+  type: "INCREMENT",
+});
+export const decrement = () => ({
+  type: "DECREMENT",
+});
